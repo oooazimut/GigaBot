8e5ba1d0d6da67f277a1026218c8af7b4f698834
oooazimut <159106807+oooazimut@users.noreply.github.com>
2024-05-21 09:50:11 +0300

    написал шаблон диалога газовых датчиков

--- dialog/g_sens.py
+++ dialog/g_sens.py
@@ -1,7 +1,7 @@
 from aiogram import F
 from aiogram_dialog import Dialog, Window, DialogManager
 from aiogram_dialog.api.entities import MediaAttachment, MediaId
-from aiogram_dialog.widgets.kbd import Start, Cancel, Back, Button, Select, Column
+from aiogram_dialog.widgets.kbd import Start, Cancel, Back, Button, Select, Column, SwitchTo
 from aiogram_dialog.widgets.media import DynamicMedia
 from aiogram_dialog.widgets.text import Const, Format
 from states import GasSensorsSG
@@ -9,15 +9,18 @@ from states import GasSensorsSG
 g_sens_menu = Dialog(
     Window(
         Const("Газоанализаторы"),
-        Button(Const("Пробная"), on_click=Start(state=GasSensorsSG.prob_sens,id="prob_bt")),
-        Button(Const("Насосная"), on_click=Start(state=GasSensorsSG.pump_sens, id="pump_id")),
+        SwitchTo(Const("Пробная"),state=GasSensorsSG.prob_sens,id="prob_bt"),
+        SwitchTo(Const("Насосная"), state=GasSensorsSG.pump_sens, id="pump_id"),
+        Cancel(Const("В меню")),
         state=GasSensorsSG.main
+
 
     ),
     Window(
         Const("Пробная"),
-        Button(Const("Текущие значения"), on_click=Start( state= GasSensorsSG.current, id="cur_bt")),
-        Button(Const("Архив"), id='achive', on_click= Start(state= GasSensorsSG.archive, id="arch_bt")),
+        SwitchTo(Const("Текущие значения"), state= GasSensorsSG.current, id="cur_bt"),
+        SwitchTo(Const("Архив"), state= GasSensorsSG.archive, id="arch_bt"),
+        Back(Const("Назад")),
         state=GasSensorsSG.prob_sens
     ),
     Window(
